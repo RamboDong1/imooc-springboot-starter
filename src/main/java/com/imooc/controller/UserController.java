091afe69b38167f622a9dce756b0caa7a90b35b8
RamboDong1 <1119745549@qq.com>
2019-07-17 19:56:43 +0800

    Create getUserJson

---
+++ src/main/java/com/imooc/controller/UserController.java
@@ -0,0 +1,44 @@
+package com.imooc.controller;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.imooc.pojo.ImoocJsonResult;
+import com.imooc.pojo.User;
+
+//@Controller
+@RestController		//@RestController = @Controller + @ResponseBody
+@RequestMapping("/user")
+public class UserController {
+
+	@RequestMapping("/getUser")
+//	@ResponseBody
+	public User getUser() {
+		User u = new User();
+		u.setName("imooc");
+		u.setAge(18);
+		u.setBirthday(new Date());
+		u.setPassword("imooc");
+		//u.setDesc(null);
+		
+		return u;
+	}
+	
+	@RequestMapping("/getUserJson")
+//	@ResponseBody
+	public ImoocJsonResult getUserJson() {
+		User u = new User();
+		u.setName("imooc");
+		u.setAge(18);
+		u.setBirthday(new Date());
+		u.setPassword("imooc");
+		//u.setDesc(null);
+		
+		return ImoocJsonResult.ok(u);
+	}
+
+}
